8cb60e3fc77adb76d0a80ddc5095995dc7c3bf9d
Iago César Fernandes Nogueira <iagocesar.nogueira@gmail.com>
2022-01-31 11:58:16 -0300

    Botão Adicionar refatorado

--- ufrmPrinc.pas
+++ ufrmPrinc.pas
@@ -40,6 +40,8 @@ type
   private
     { Private declarations }
     function Padr(texto : String;Tam : Integer):String;
+    function RetornaPrefixo: string;
+    function RetornaSufixo: string;
 
   public
     { Public declarations }
@@ -59,39 +61,46 @@ implementation
 
 {$R *.dfm}
 
+function TfrmPrinc.RetornaPrefixo: string;
+begin
+  if cmbClasse.ItemIndex = opTStrings then
+    Result := edtVariavel.Text + '.Add('+#39
+
+  else if cmbClasse.ItemIndex = opString then
+    Result := edtVariavel.Text + ' := '+edtVariavel.Text+' + '+ #39
+
+  else
+    result := edtVariavel.Text + '.SQL.Add('+#39;
+end;
+
+function TfrmPrinc.RetornaSufixo: string;
+begin
+  if cmbClasse.ItemIndex = opString then
+    Result := #39 +';'
+  else
+    Result := #39+');';
+end;
+
 procedure TfrmPrinc.btnAdicionarClick(Sender: TObject);
 var
   r, i, qtd     : integer;
   s, idn, sTemp : string;
   slParam       : TStrings;
 
-  function NovaLinha(trecho:string): string;
-  begin
-    result := '';
-    if cmbClasse.ItemIndex = opTStrings then
-      mmPascal.Lines.Add(idn + edtVariavel.Text + '.Add(''' + sTemp + ' '');' )
-
-    else if cmbClasse.ItemIndex = opString then
-      mmPascal.Lines.Add(idn + edtVariavel.Text + ' := '+edtVariavel.Text+' + '+ #39 + sTemp + #39 +';' )
-
-    else
-      mmPascal.Lines.Add(idn + edtVariavel.Text + '.SQL.Add(''' + sTemp + ' '');' );
-  end;
-
   procedure ExtraiParametro(sAux : string; slAux : TStrings);
   var c,i : integer; tmp : string;
   begin
-  c := pos(':',sAux); i := pos(' ',sAux,c);
-  tmp := copy (sAux,c+1,i-c-1);
-  if tmp <> '' then
-  begin
-  if tmp[Length(tmp)] = ',' then Delete(tmp, Length(tmp), 1);
-  if tmp[Length(tmp)] = ')' then Delete(tmp, Length(tmp), 1);
-  if pos(tmp,slAux.Text) = 0 then slAux.Add(tmp);
-  end;
-  sAux := copy(sAux,pos(' ',sAux,c),Length(sAux));
-  c := pos(':',sAux);
-  if c > 0 then ExtraiParametro(sAux,slAux);
+    c := pos(':',sAux); i := pos(' ',sAux,c);
+    tmp := copy (sAux,c+1,i-c-1);
+    if tmp <> '' then
+    begin
+    if tmp[Length(tmp)] = ',' then Delete(tmp, Length(tmp), 1);
+    if tmp[Length(tmp)] = ')' then Delete(tmp, Length(tmp), 1);
+    if pos(tmp,slAux.Text) = 0 then slAux.Add(tmp);
+    end;
+    sAux := copy(sAux,pos(' ',sAux,c),Length(sAux));
+    c := pos(':',sAux);
+    if c > 0 then ExtraiParametro(sAux,slAux);
   end;
 begin
    mmPascal.Clear;
@@ -132,7 +141,10 @@ begin
             mmPascal.Lines.Add(' ')
           else begin
             sTemp := StringReplace(sTemp, #39, #39#39, [rfReplaceAll]);
-            mmPascal.Lines.Add(NovaLinha(sTemp));
+
+            mmPascal.Lines.Add(
+              idn + RetornaPrefixo + sTemp + RetornaSufixo
+            );
 
             //verificando se existe parâmetro na linha atual
             if pos(':',sTemp) > 0 then
